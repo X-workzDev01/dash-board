feae059e097c1b7ba03aa61fc07546628a5e4bd0
X-workzDev01 <73932465+X-workzDev01@users.noreply.github.com>
2024-01-05 19:26:47 +0530

    hosting on xworkz

---
+++ data.js
@@ -0,0 +1,114 @@
+
+let batchList = [
+    // <!-- //om,abhish,suhas.satish,dev,lokesh ,exception vinoda,javascript -hareesha -->
+    {
+        "batch": "June-5 Rajajinagar",
+        "trainer": "Devendra",
+        "topic": "Java 8 features",
+        "timing":"10:00 AM",
+        "trainees":"27"
+    },
+    {
+        "batch": "June-7 BTM",
+        "trainer": "Suhas",
+        "topic": "Project",
+        "timing":"10:00 AM",
+        "trainees":"14"
+    },
+    {
+        "batch": "June-19 Rajajinagar",
+        "trainer": "Omkar",
+        "topic": "Spring MVC",
+        "timing":"4:00 PM",
+        "trainees":"30"
+    },
+    {
+        "batch": "July-24 Rajajinagar",
+        "trainer": "Lokesh",
+        "topic": "JDBC",
+        "timing":"1:45 PM",
+        "trainees":"23"
+    },
+    {
+        "batch": "Aug-21 Rajajinagar",
+        "trainer": "Vinoda",
+        "topic": "Exception",
+        "timing":"6:00 PM",
+        "trainees":"11"
+    },
+    {
+        "batch": "Aug-21 Rajajinagar",
+        "trainer": "Hareesha",
+        "topic": "Javascript",
+        "timing":"4:00 PM",
+        "trainees":"11"
+    },
+    {
+        "batch": "Sep-27 BTM",
+        "trainer": "Omkar",
+        "topic": "Exception",
+        "timing":"12:00 PM",
+        "trainees":"19"
+    }, {
+        "batch": "Nov-27 BTM",
+        "trainer": "Abhishek",
+        "topic": "Static methods",
+        "timing":"11:00 AM",
+        "trainees":"13"
+    },
+    
+    {
+        "batch": "Online",
+        "trainer": "Satish",
+        "topic": "SQL",
+        "timing":"8:30 AM",
+        "trainees":"7"
+    }
+    
+    
+]
+
+// let weeklyMock = [
+
+//     {
+//         "batch": "june 5",
+//         "mock_spoc": "Hareesha",
+//         "topic": "collection"
+//     }, {
+//         "batch": "nov",
+//         "mock_spoc": "old students",
+//         "topic": "exception"
+//     },
+//     {
+//         "batch": "nov",
+//         "mock_spoc": "om sir",
+//         "topic": "exception"
+//     }
+// ]
+
+
+
+// let upcomingBatch = [
+
+//     {
+//         "batch": "jan 5",
+//         "trainer": "dev sir",
+//         "location": "Rajajinagar"
+//     }, {
+//         "batch": "feb 10",
+//         "trainer": "om sir",
+//         "location": "Rajajinagar"
+//     },
+//     {
+//         "batch": "feb 12",
+//         "trainer": "hareesha",
+//         "location": "BTM"
+//     }
+// ]
+
+
+
+
+
+
+
