4ccc46640337092d0fa3b4edd2f23e18d35e8abb
x-workzdev01 <murtuza.xworkz@gmail.com>
2024-06-10 14:30:58 +0530

    added new batch

--- data.js
+++ data.js
@@ -1,45 +1,58 @@
 
 let batchList = [
- {
+    {
         "batch": "CH10ROM",
         "trainer": "Devendra",
         "topic": "Basic Java",
         "timing": "2:30 AM",
-        "trainees": "45"
+        "trainees": "45",
+        "location": "Rajajinagar"
+    }, {
+        "batch": "CH10BOM",
+        "trainer": "Vinoda",
+        "topic": "Basic Java",
+        "timing": "10:00 AM",
+        "trainees": "20",
+        "location": "BTM"
     },
     {
         "batch": "CH03ROM",
         "trainer": "Devendra",
         "topic": "Basic Java",
         "timing": "11:30 AM",
-        "trainees": "98"
+        "trainees": "98",
+        "location": "Rajajinagar"
     },
     {
         "batch": "Nitte-meenakshi CSR",
         "trainer": "Hareesha",
         "topic": "Web technologies",
         "timing": "10:00 AM",
-        "trainees": "21"
+        "trainees": "21",
+        "location": "Rajajinagar"
     }, {
         "batch": "March-26 Rajajinagar",
         "trainer": "Chetan",
         "topic": "Variables",
         "timing": "6:00 PM",
-        "trainees": "5"
+        "trainees": "5",
+        "location": "Rajajinagar"
     },
     {
         "batch": "Online Feb-5",
         "trainer": "Satish",
         "topic": "OOPS",
         "timing": "7:00 PM",
-        "trainees": "7"
+        "trainees": "7",
+        "location": "Online"
     },
     {
         "batch": "Jan-29 Rajajinagar",
         "trainer": "Devendra",
-        "topic": "Exception",
+        "topic": "Collection",
         "timing": "9:00 AM",
-        "trainees": "8"
+        "trainees": "8",
+        "location": "Rajajinagar"
     },
 
     {
@@ -47,14 +60,16 @@ let batchList = [
         "trainer": "Devendra",
         "topic": "Project Completed",
         "timing": "11:30 AM",
-        "trainees": "23"
+        "trainees": "23",
+        "location": "Rajajinagar"
     },
     {
-        "batch": "Nov-27 BTM",
+        "batch": "Sep-27 BTM",
         "trainer": "Omkar",
         "topic": "Project",
         "timing": "11:00 AM",
-        "trainees": "13"
+        "trainees": "13",
+        "location": "BTM"
     }
 
 ]
